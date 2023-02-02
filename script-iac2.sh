1c56284490ee08143814c7e3880042a40d0bf991
wsantos-cyber <68574194+wsantos-cyber@users.noreply.github.com>
2023-02-02 19:02:55 -0300

    Update script-iac2.sh

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Atualizando o servidor..."
+echo "Automacao de atualizacao do servidor..."
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
 
-echo "Baixando e copiando os arquivos da aplicação..."
+echo "Baixando e copiando os arquivos da aplicação (PENDENTE ALTERACAO)..."
 
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
